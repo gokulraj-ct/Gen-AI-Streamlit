d9f38f406019d3ef892e163dbf9d70ed3bb35e96
gokulraj-ct <gokul.r@cloudthat.com>
2025-06-30 11:51:30 +0530

    app.py modified

--- app.py
+++ app.py
@@ -5,26 +5,29 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 import pandas as pd
 import pickle
 
-# Load the trained model
-model = tf.keras.models.load_model('model.h5')
+@st.cache_resource
+def load_model():
+    return tf.keras.models.load_model('churn_model.h5')
 
-# Load the encoders and scaler
-with open('label_encoder_gender.pkl', 'rb') as file:
-    label_encoder_gender = pickle.load(file)
+@st.cache_resource
+def load_encoders():
+    with open('onehot_encoder_geo.pkl', 'rb') as file:
+        onehot_encoder_geo = pickle.load(file)
+    with open('scaler.pkl', 'rb') as file:
+        scaler = pickle.load(file)
+    return onehot_encoder_geo, scaler
 
-with open('onehot_encoder_geo.pkl', 'rb') as file:
-    onehot_encoder_geo = pickle.load(file)
-
-with open('scaler.pkl', 'rb') as file:
-    scaler = pickle.load(file)
+# Load the trained model and encoders
+model = load_model()
+onehot_encoder_geo, scaler = load_encoders()
 
 
 ## streamlit app
-st.title('Customer Churn PRediction')
+st.title('Customer Churn Prediction')
 
 # User input
 geography = st.selectbox('Geography', onehot_encoder_geo.categories_[0])
-gender = st.selectbox('Gender', label_encoder_gender.classes_)
+gender = st.selectbox('Gender', ['Male', 'Female'])
 age = st.slider('Age', 18, 92)
 balance = st.number_input('Balance')
 credit_score = st.number_input('Credit Score')
@@ -37,7 +40,7 @@ is_active_member = st.selectbox('Is Active Member', [0, 1])
 # Prepare the input data
 input_data = pd.DataFrame({
     'CreditScore': [credit_score],
-    'Gender': [label_encoder_gender.transform([gender])[0]],
+    'Gender': [1 if gender == 'Male' else 0],
     'Age': [age],
     'Tenure': [tenure],
     'Balance': [balance],
@@ -48,7 +51,7 @@ input_data = pd.DataFrame({
 })
 
 # One-hot encode 'Geography'
-geo_encoded = onehot_encoder_geo.transform([[geography]]).toarray()
+geo_encoded = onehot_encoder_geo.transform([[geography]])
 geo_encoded_df = pd.DataFrame(geo_encoded, columns=onehot_encoder_geo.get_feature_names_out(['Geography']))
 
 # Combine one-hot encoded columns with input data
